e74de900492670803dc3a98e8723f86d661bb08f
Jeroen Claassens <support@favware.tech>
2021-08-22 22:17:45 +0200

    feat: update twitch to eventsub (#2010)
    Co-authored-by: Antonio Román <kyradiscord@gmail.com>
    Co-authored-by: favware-bot <favwarebot@gmail.com>

--- src/listeners/twitch/twitchStreamOffline.ts
+++ src/listeners/twitch/twitchStreamOffline.ts
@@ -1,55 +1,61 @@
-import { GuildSettings, NotificationsStreamsTwitchEventStatus, readSettings } from '#lib/database';
 import { LanguageKeys } from '#lib/i18n/languageKeys';
-import type { PostStreamBodyData } from '#root/routes/twitch/twitchStreamChange';
+import { TwitchEventSubEvent, TwitchEventSubTypes } from '#lib/types';
+import { Events } from '#lib/types/Enums';
 import { floatPromise } from '#utils/common';
-import { canSendMessages } from '@sapphire/discord.js-utilities';
-import { Listener } from '@sapphire/framework';
-import type { ApiResponse } from '@sapphire/plugin-api';
-import { MessageEmbed, TextChannel } from 'discord.js';
+import { ApplyOptions } from '@sapphire/decorators';
+import { canSendMessages, TextBasedChannelTypes } from '@sapphire/discord.js-utilities';
+import { Listener, ListenerOptions } from '@sapphire/framework';
+import { fetchT } from '@sapphire/plugin-i18next';
+import { isNullish } from '@sapphire/utilities';
+import { MessageEmbed } from 'discord.js';
 import type { TFunction } from 'i18next';
 
-export class UserListener extends Listener {
-	public async run(data: PostStreamBodyData, response: ApiResponse) {
-		// Fetch the streamer, and if it could not be found, return error.
-		const { twitchStreamSubscriptions } = this.container.db;
-		const streamer = await twitchStreamSubscriptions.findOne({ id: data.id });
-		if (!streamer) return response.error('No streamer could be found in the database.');
+@ApplyOptions<ListenerOptions>({
+	event: Events.TwitchStreamOffline
+})
+export class UserListener extends Listener<Events.TwitchStreamOffline> {
+	public async run(data: TwitchEventSubEvent) {
+		const { twitchSubscriptions } = this.container.db;
 
-		// Iterate over all the guilds that are subscribed to the streamer.
-		for (const guildId of streamer.guildIds) {
-			// Retrieve the guild, if not found, skip to the next loop cycle.
-			const guild = this.container.client.guilds.cache.get(guildId);
-			if (typeof guild === 'undefined') continue;
+		const twitchSubscription = await twitchSubscriptions.findOne({
+			relations: ['guildSubscription'],
+			where: {
+				streamerId: data.broadcaster_user_id,
+				subscriptionType: TwitchEventSubTypes.StreamOffline
+			}
+		});
 
-			// Synchronize the settings, then retrieve to all of its subscriptions
-			const [allSubscriptions, t] = await readSettings(guild, (settings) => [
-				settings[GuildSettings.Notifications.Stream.Twitch.Streamers],
-				settings.getLanguage()
-			]);
-
-			const subscriptions = allSubscriptions.find(([id]) => id === streamer.id);
-			if (typeof subscriptions === 'undefined') continue;
-
-			// Iterate over each subscription
-			for (const subscription of subscriptions[1]) {
-				if (subscription.status !== NotificationsStreamsTwitchEventStatus.Offline) continue;
-				if (this.container.client.twitch.streamNotificationDrip(`${subscriptions[0]}-${subscription.channel}-${subscription.status}`))
+		if (twitchSubscription) {
+			// Iterate over all the guilds that are subscribed to this streamer and subscription type
+			for (const guildSubscription of twitchSubscription.guildSubscription) {
+				if (
+					this.container.client.twitch.streamNotificationDrip(
+						`${twitchSubscription.streamerId}-${guildSubscription.channelId}-${TwitchEventSubTypes.StreamOffline}`
+					)
+				) {
 					continue;
-
-				// Retrieve the channel, then check if it exists or if it's postable.
-				const channel = guild.channels.cache.get(subscription.channel) as TextChannel | undefined;
-				if (channel === undefined || !canSendMessages(channel)) continue;
-
-				// If the message could not be retrieved then skip this notification.
-				if (subscription.message) {
-					floatPromise(channel.send({ embeds: [this.buildEmbed(subscription.message, t)] }));
 				}
 
-				break;
+				// Retrieve the guild, if not found, skip to the next loop cycle.
+				const guild = this.container.client.guilds.cache.get(guildSubscription.guildId);
+				if (typeof guild === 'undefined') continue;
+
+				// Retrieve the language for this guild
+				const t = await fetchT(guild);
+
+				// Retrieve the channel to send the message to
+				const channel = guild.channels.cache.get(guildSubscription.channelId) as TextBasedChannelTypes;
+				if (isNullish(channel) || !canSendMessages(channel)) {
+					continue;
+				}
+
+				// Construct a message embed and send it.
+				// If the message could not be retrieved then skip this notification.
+				if (guildSubscription.message) {
+					floatPromise(channel.send({ embeds: [this.buildEmbed(guildSubscription.message, t)] }));
+				}
 			}
 		}
-
-		return response.ok();
 	}
 
 	private buildEmbed(message: string, t: TFunction) {
--- src/listeners/twitch/twitchStreamOnline.ts
+++ src/listeners/twitch/twitchStreamOnline.ts
@@ -1,112 +1,119 @@
-import { GuildSettings, NotificationsStreamsTwitchEventStatus, readSettings } from '#lib/database';
 import { LanguageKeys } from '#lib/i18n/languageKeys';
-import type { TwitchHelixGameSearchResult } from '#lib/types/definitions/Twitch';
-import type { PostStreamBodyData } from '#root/routes/twitch/twitchStreamChange';
+import { TwitchEventSubTypes } from '#lib/types';
+import type { TwitchEventSubOnlineEvent, TwitchHelixStreamsResult, TwitchOnlineEmbedData } from '#lib/types/definitions/Twitch';
+import { Events } from '#lib/types/Enums';
 import { floatPromise } from '#utils/common';
 import { escapeMarkdown } from '#utils/External/escapeMarkdown';
-import { canSendMessages } from '@sapphire/discord.js-utilities';
-import { Listener } from '@sapphire/framework';
-import type { ApiResponse } from '@sapphire/plugin-api';
-import { MessageEmbed, TextChannel } from 'discord.js';
+import { ApplyOptions } from '@sapphire/decorators';
+import { canSendMessages, TextBasedChannelTypes } from '@sapphire/discord.js-utilities';
+import { Listener, ListenerOptions } from '@sapphire/framework';
+import { fetchT } from '@sapphire/plugin-i18next';
+import { isNullish } from '@sapphire/utilities';
+import { MessageEmbed } from 'discord.js';
 import type { TFunction } from 'i18next';
 
-export class UserListener extends Listener {
+@ApplyOptions<ListenerOptions>({
+	event: Events.TwitchStreamOnline
+})
+export class UserListener extends Listener<Events.TwitchStreamOnline> {
 	private readonly kTwitchImageReplacerRegex = /({width}|{height})/gi;
 
-	public async run(data: PostStreamBodyData, response: ApiResponse) {
-		// All streams should have a game_id.
-		if (typeof data.game_id === 'undefined') return response.error('"game_id" field is not defined.');
+	public async run(data: TwitchEventSubOnlineEvent) {
+		const { twitchSubscriptions } = this.container.db;
 
-		// Fetch the streamer, and if it could not be found, return error.
-		const { twitchStreamSubscriptions } = this.container.db;
-		const streamer = await twitchStreamSubscriptions.findOne({ id: data.user_id });
-		if (!streamer) return response.error('No streamer could be found in the database.');
+		const twitchSubscription = await twitchSubscriptions.findOne({
+			relations: ['guildSubscription'],
+			where: {
+				streamerId: data.broadcaster_user_id,
+				subscriptionType: TwitchEventSubTypes.StreamOnline
+			}
+		});
 
-		const {
-			data: [game]
-		} = await this.container.client.twitch.fetchGame([data.game_id]);
-		// Iterate over all the guilds that are subscribed to the streamer.
-		for (const guildId of streamer.guildIds) {
-			// Retrieve the guild, if not found, skip to the next loop cycle.
-			const guild = this.container.client.guilds.cache.get(guildId);
-			if (typeof guild === 'undefined') continue;
+		if (twitchSubscription) {
+			try {
+				// Get the data for this stream from the Twitch API
+				const streamData = await this.container.client.twitch.fetchStream(data.broadcaster_user_id);
 
-			// Synchronize the settings, then retrieve to all of its subscriptions
-			const [allSubscriptions, t] = await readSettings(guild, (settings) => [
-				settings[GuildSettings.Notifications.Stream.Twitch.Streamers],
-				settings.getLanguage()
-			]);
-
-			const subscriptions = allSubscriptions.find(([id]) => id === streamer.id);
-			if (typeof subscriptions === 'undefined') continue;
-
-			// Iterate over each subscription
-			for (const subscription of subscriptions[1]) {
-				if (subscription.status !== NotificationsStreamsTwitchEventStatus.Online) continue;
-				if (game !== undefined) {
-					if (subscription.gamesBlacklist.includes(game.name) || subscription.gamesBlacklist.includes(game.id)) continue;
+				// Iterate over all the guilds that are subscribed to this streamer and subscription type
+				for (const guildSubscription of twitchSubscription.guildSubscription) {
 					if (
-						subscription.gamesWhitelist.length &&
-						(!subscription.gamesWhitelist.includes(game.name) || !subscription.gamesWhitelist.includes(game.id))
-					)
+						this.container.client.twitch.streamNotificationDrip(
+							`${twitchSubscription.streamerId}-${guildSubscription.channelId}-${TwitchEventSubTypes.StreamOnline}`
+						)
+					) {
 						continue;
+					}
+
+					// Retrieve the guild, if not found, skip to the next loop cycle.
+					const guild = this.container.client.guilds.cache.get(guildSubscription.guildId);
+					if (typeof guild === 'undefined') continue;
+
+					// Retrieve the language for this guild
+					const t = await fetchT(guild);
+
+					// Retrieve the channel to send the message to
+					const channel = guild.channels.cache.get(guildSubscription.channelId) as TextBasedChannelTypes;
+					if (isNullish(channel) || !canSendMessages(channel)) {
+						continue;
+					}
+
+					// Construct a message embed and send it.
+					floatPromise(
+						channel.send({
+							content: guildSubscription.message || null,
+							embeds: [this.buildEmbed(this.transformTextToObject(data, streamData), t)]
+						})
+					);
 				}
-				if (this.container.client.twitch.streamNotificationDrip(`${subscriptions[0]}-${subscription.channel}-${subscription.status}`))
-					continue;
-
-				// Retrieve the channel, then check if it exists or if it's postable.
-				const channel = guild.channels.cache.get(subscription.channel) as TextChannel | undefined;
-				if (channel === undefined || !canSendMessages(channel)) continue;
-
-				// Construct a message embed and send it.
-				floatPromise(channel.send({ embeds: [this.buildEmbed(this.transformTextToObject(data, game), t)] }));
-
-				break;
+			} catch {
+				// noop, this try/catch is only here so we don't get a runtime error.
 			}
 		}
-
-		return response.ok();
 	}
 
-	private transformTextToObject(notification: PostStreamBodyData, game?: TwitchHelixGameSearchResult): TwitchOnlineEmbedData {
+	private transformTextToObject(notification: TwitchEventSubOnlineEvent, streamData: TwitchHelixStreamsResult | null): TwitchOnlineEmbedData {
 		return {
-			user_name: notification.user_name,
-			title: this.escapeText(notification.title),
-			started_at: new Date(notification.started_at),
-			viewer_count: notification.viewer_count.toString(),
-			language: notification.language,
-			thumbnail_url: notification.thumbnail_url.replace(this.kTwitchImageReplacerRegex, '128'),
-			type: notification.type,
-			game_name: game?.name,
-			box_art_url: game?.box_art_url?.replace(this.kTwitchImageReplacerRegex, '1024'),
-			tag_ids: notification.tag_ids,
-			user_id: notification.user_id
+			embedThumbnailUrl: streamData?.game_box_art_url?.replace(this.kTwitchImageReplacerRegex, '128'),
+			gameName: streamData?.game_name,
+			language: streamData?.language,
+			startedAt: new Date(notification.started_at),
+			title: this.escapeText(streamData?.title),
+			userName: notification.broadcaster_user_name,
+			viewerCount: streamData?.viewer_count,
+			embedImageUrl: streamData?.thumbnail_url.replace(this.kTwitchImageReplacerRegex, '128')
 		};
 	}
 
 	private buildEmbed(data: TwitchOnlineEmbedData, t: TFunction) {
-		return new MessageEmbed()
-			.setThumbnail(data.thumbnail_url)
+		const embed = new MessageEmbed()
 			.setTitle(data.title)
-			.setURL(`https://twitch.tv/${data.user_name}`)
-			.setDescription(
-				data.game_name
-					? t(LanguageKeys.Events.Twitch.EmbedDescriptionWithGame, { userName: data.user_name, gameName: data.game_name })
-					: t(LanguageKeys.Events.Twitch.EmbedDescription, { userName: data.user_name })
-			)
+			.setURL(`https://twitch.tv/${data.userName}`)
 			.setFooter(t(LanguageKeys.Events.Twitch.EmbedFooter))
-			.setTimestamp(data.started_at)
 			.setColor(this.container.client.twitch.BRANDING_COLOUR)
-			.setImage(data.box_art_url ?? '');
+			.setTimestamp(data.startedAt);
+
+		if (data.gameName) {
+			embed.setDescription(t(LanguageKeys.Events.Twitch.EmbedDescriptionWithGame, { userName: data.userName, gameName: data.gameName }));
+		} else {
+			embed.setDescription(t(LanguageKeys.Events.Twitch.EmbedDescription, { userName: data.userName }));
+		}
+
+		if (data.embedImageUrl) {
+			embed.setImage(data.embedImageUrl);
+		}
+
+		if (data.embedThumbnailUrl) {
+			embed.setThumbnail(data.embedThumbnailUrl ?? '');
+		}
+
+		return embed;
 	}
 
-	private escapeText(text: string) {
+	private escapeText(text?: string) {
+		if (isNullish(text)) {
+			return '';
+		}
+
 		return escapeMarkdown(text.replace(/\\/g, '\\\\').replace(/"/g, '\\"'));
 	}
 }
-
-type TwitchOnlineEmbedData = Omit<PostStreamBodyData, 'id' | 'viewer_count' | 'started_at' | 'game_id' | 'user_login'> &
-	Omit<Partial<TwitchHelixGameSearchResult>, 'id' | 'name'> & {
-		viewer_count: string;
-		started_at: Date;
-	};
